fe34469a4437776642b16a8906419f020fc32c3d
Nguyen Hua <nguyenhua.hcm@gmail.com>
2024-04-30 23:07:03 +0700

    Implement address in searchbar and hide searchbar for company edit profile page

--- app/views/components/search_bar.php
+++ app/views/components/search_bar.php
@@ -6,17 +6,14 @@
       </span>
       <input type="text" class="form-control" placeholder="Search jobs..." aria-label="seach-query" aria-describedby="addon-wrapping">
     </div>
-    <select class="selectpicker" title="Location" data-live-search="true" data-allow-clear="true">
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+    <select class="selectpicker" name="location" title="Location" id="city-picker" data-live-search="true" data-allow-clear="true">
     </select>
-    <select class="selectpicker" title="Work arrangements" data-allow-clear="true">
+    <select class="selectpicker" name="work_arrangement" title="Work arrangements" data-allow-clear="true">
       <option value="onsite">On site</option>
       <option value="remote">Remote</option>
       <option value="hybrid">Hybrid</option>
     </select>
-    <select class="selectpicker" title="Levels" data-allow-clear="true">
+    <select class="selectpicker" name="level" title="Levels" data-allow-clear="true">
       <option value="intern">Intern/Student</option>
       <option value="fresher">Fresher/Entry level</option>
       <option value="experienced">Experienced (non-manager)</option>
@@ -29,5 +26,24 @@
 <script>
 $(document).ready(function () {
   $('select').selectpicker();
+  $.ajax({
+    url: "http://localhost:8080/data/address.json",
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+      cities = Object.values(data);
+      const citiesOptions = cities.map(city => ({
+        value: city.slug,
+        option: city.name_with_type
+      }))
+      $.each(citiesOptions, function(_, item) {
+        $("#city-picker").append($('<option>', {
+          value: item.value,
+          text: item.option
+        }))
+      }) 
+      $('#city-picker').selectpicker('refresh');
+    }
+  })
 })
 </script>
